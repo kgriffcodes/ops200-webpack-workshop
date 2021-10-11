b2358c442e388a6232796581495dff77c5b5c3c2
kgriffcodes <kgriffith1214@gmail.com>
2021-10-11 09:58:48 -0500

    add project files

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,22 @@
 const path = require('path');
 
-module.exports = {};
+module.exports = {
+  mode: 'none',
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    filename: './dist/bundle.js/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
+
